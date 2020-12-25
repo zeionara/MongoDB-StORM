6d334ed47d7d1dc2cddc3e8118acab95395950ca
Dmitrii <zeionara@gmail.com>
2020-12-25 18:03:54 +0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 let package = Package(
@@ -8,9 +8,9 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/SwiftORM/StORM.git", from: "3.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+		.package(name: "PerfectLogger", url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
 		.package(url: "https://github.com/iamjono/SwiftRandom.git", from: "1.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-MongoDB.git", from: "3.0.0"),
+		.package(name: "PerfectMongoDB", url: "https://github.com/zeionara/Perfect-MongoDB.git", .branch("master")),
 	],
 	targets: [
 		.target(
